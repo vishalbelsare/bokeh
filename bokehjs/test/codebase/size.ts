fa837e09e405f9b8b11a860ce86940fd15094d5c
Mateusz Paprocki <mattpap@gmail.com>
2021-06-22 19:13:52 +0200

    Drop support for IE and legacy web browsers (#10931)
    * Drop support for IE & obsolete web browsers
    
    * Simplify is_* type checkers
    
    * Drop support for ES5 target
    
    * Update test/codebase/size
    
    * Drop Context2d.setTransform() workaround
    
    * Remove legacy resources
    
    * Update bokehjs/package-lock.json

--- bokehjs/test/codebase/size.ts
+++ bokehjs/test/codebase/size.ts
@@ -10,18 +10,14 @@ const LIMITS = new Map([
   ["js/bokeh-widgets.min.js",         300],
   ["js/bokeh-tables.min.js",          350],
   ["js/bokeh-api.min.js",             100],
-  // legacy (es5)
-  ["js/bokeh.legacy.min.js",         1200],
-  ["js/bokeh-widgets.legacy.min.js",  350],
-  ["js/bokeh-tables.legacy.min.js",   350],
-  ["js/bokeh-api.legacy.min.js",      110],
 ])
 
 const n = Math.max(...[...LIMITS.keys()].map((l) => l.length))
+const k = Math.max(...[...LIMITS.values()].map((s) => `${s}`.length))
 
 function pad(value: unknown): string {
   const str = `${value}`
-  const pre = " ".repeat(4 - str.length)
+  const pre = " ".repeat(k - str.length)
   return `${pre}${str}`
 }
 
